e092255f4764f1f38884291fe4f99078deb7a04a
AllsanDev <97900721+AllsanDev@users.noreply.github.com>
2024-09-04 13:34:46 +0200

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+<v-container class="hello">
+  <div>moi</div>
+  <div>skills</div>
+  <div>projets</div>
+  <div>contact</div>
+</v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hello {
+  height: 100%;
+  background: #b98942;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
