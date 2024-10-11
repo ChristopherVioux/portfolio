37961a6e92c1df6813007424a80f57cf6fec548c
Chris Vioux <christophervioux@gmail.com>
2024-10-11 21:35:46 +0200

    v1 go

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -5,24 +5,25 @@
       <router-link class="nav" to="/#/account">Home</router-link>
     </nav>
     <div class="contact-form">
+      <div class="contact-title">Contact</div>
       <form @submit.prevent="handleSubmit" class="form">
         <div class="contactSection">
-          <label for="name">Name :</label>
-          <input type="text" id="name" v-model="form.name" />
+          <label for="name"></label>
+          <input class="appForm" placeholder="Name" type="text" id="name" v-model="form.name" />
         </div>
         <div class="contactSection">
-          <label for="phone">Phone :</label>
-          <input type="text" id="phone" v-model="form.phone" />
+          <label for="phone"></label>
+          <input class="appForm" placeholder="Phone" type="text" id="phone" v-model="form.phone" />
         </div>
         <div class="contactSection">
-          <label for="email">Email :</label>
-          <input type="email" id="email" v-model="form.email" />
+          <label for="email"></label>
+          <input class="appForm" placeholder="Email" type="email" id="email" v-model="form.email" />
         </div>
         <div class="contactSection">
-          <label for="message">Message :</label>
-          <textarea id="message" v-model="form.message" />
+          <label for="message"></label>
+          <textarea class="appForm" placeholder="Message" id="message" v-model="form.message" />
         </div>
-        <button type="submit">Envoyer</button>
+        <button class="app-form-button" type="submit">SEND</button>
       </form>
     </div>
   </v-container>
@@ -63,24 +64,32 @@ export default defineComponent({
 });
 </script>
 
-<style>
+<style scoped>
 .nav {
   color: #fffdec;
+  margin-right:25px;
 }
 .navbar {
   background-color: #0A001B;
   display: flex;
   justify-content: flex-end;
+  width: 95%;
 }
 .contact-form {
-  height: 100%;
+  height: 70%;
+  width: 45%;
+  background: #1b083a;
+  border-radius: 5px;;
+  display: flex;
+  flex-direction: row;
+  align-items: center
 }
 .contactSection {
   display: flex;
   color: white;
   font-size: 1.3rem;
   flex-direction: column;
-  width: 30%;
+  width: 60%;
   align-content: center;
   justify-content: center;
   margin-top: 20px;
@@ -91,10 +100,52 @@ export default defineComponent({
   overflow-x:hidden;
   height: 100vh;
   width: 100%;
+  align-items: center;
+}
+.contact-title {
+  width:50%;
+  font-size: 2.5em;
+  font-weight: bold;
+  margin-bottom:190px;
+  color: #ea1d6f;
 }
 .form {
   display: flex;
   flex-direction: column;
   align-items: center;
+  width: 50%;
+}
+.app-form-button {
+  background: none;
+  font-weight: bold;
+  border: none;
+  font-size: 1em;
+  color: #ea1d6f;
+  margin-top:15px;
+  cursor: pointer;
+  outline: none;
+}
+.appForm {
+  width: 100%;
+  padding: 10px 0;
+  background: none;
+  border: none;
+  border-bottom: 1px solid #666;
+  color: #ddd;
+  font-size: 14px;
+  text-transform: uppercase;
+  outline: none;
+  transition: border-color .2s;
+}
+
+.appForm::placeholder {
+  color: #666;
+}
+
+.appForm:focus {
+  border-bottom-color: #ddd;
+}
+.app-form-button:hover {
+  color: #b9134f;
 }
 </style>
